f9ff1c1770cd66ecfef6443033864164e2a2e1f8
UlinduRasantha <rasanthaulindu8888@gmail.com>
2025-08-14 20:18:41 +0530

    update

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -7,6 +7,7 @@ class WelcomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    ///
     return Scaffold(
       body: SingleChildScrollView(
         // Add SingleChildScrollView to prevent overflow
